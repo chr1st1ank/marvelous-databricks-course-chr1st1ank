ed693122e9d1954404840ef452533f04e491e803
chr1st1ank <chr1st1ank@krudewig-online.de>
2025-02-26 21:18:38 +0100

    more linting errors

--- booking/preprocessing.py
+++ booking/preprocessing.py
@@ -1,9 +1,10 @@
-
 def load_csv(spark, data_file):
     return spark.read.csv(data_file, header=True)
 
+
 def train_test_split(df, test_ratio=0.2):
-    return df.randomSplit([1-test_ratio, test_ratio])
+    return df.randomSplit([1 - test_ratio, test_ratio])
+
 
 def save_to_catalog(df, catalog_name, schema_name, table_name):
     df.write.mode("overwrite").saveAsTable(".".join([catalog_name, schema_name, table_name]))
